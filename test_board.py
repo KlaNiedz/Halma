d7c07365d65554aea88a41187c4bd6133d8c3056
kniedzia <klaudianiedzi@gmail.com>
2023-12-30 18:05:02 +0100

    improved names in test_board

--- test_board.py
+++ test_board.py
@@ -5,35 +5,35 @@ from piece import Piece
 
 def test_create_board_object():
     # PIECES = 6
-    board_object = Board()
-    assert board_object.red_left == 6
-    assert board_object.green_left == 6
-    assert board_object.winner_green == 0
-    assert board_object.winner_red == 0
+    board = Board()
+    assert board.red_left == 6
+    assert board.green_left == 6
+    assert board.winner_green == 0
+    assert board.winner_red == 0
 
-# board_object.board[0][0] = Piece(11, 11, BUTTON_WHEELUP)
-    # assert repr(board_object.board[0][11]) == '(0, 204, 0)'
+    # board.board[0][0] = Piece(11, 11, BUTTON_WHEELUP)
+    # assert repr(board.board[0][11]) == '(0, 204, 0)'
 
 
 def test_create_board_table():
-    board_obj = Board()
-    piece = board_obj.board[0][COLS-1]
+    board = Board()
+    piece = board.board[0][COLS-1]
     assert isinstance(piece, Piece)
 
 
 def test_check_winner():
-    halma_board = Board()
+    board = Board()
     for row in range(ROWS):
         for col in range(COLS):
             PIECES == 6
             max_piec_in_row = PIECES//2
             if col >= COLS - max_piec_in_row and row < max_piec_in_row:
                 if col >= COLS - max_piec_in_row + row:
-                    halma_board.board[row][col] = Piece(row, col, RED)
+                    board.board[row][col] = Piece(row, col, RED)
 
-    halma_board.check_winner()
-    assert halma_board.winner_red == 6
-    assert halma_board.check_winner() == "RED WON"
+    board.check_winner()
+    assert board.winner_red == 6
+    assert board.check_winner() == "RED WON"
 
 
 def test_get_moves():
@@ -42,8 +42,8 @@ def test_get_moves():
     # SQUARE_SIZE = WIDTH//COLS
     # # 3 possible amount of pieces: 6, 13, 19
     # PIECES = 6
-    board_obj = Board()
-    result = board_obj._left(2, 0, -1, GREEN, 0, skipped=[])
+    board = Board()
+    result = board._left(2, 0, -1, GREEN, 0, skipped=[])
     assert result == {(0, 2): []}
 
 
